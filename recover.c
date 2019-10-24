e950f4a0b7a1bfbaa7a298754c5fb92cdc99a14b
Alekky09 <Alekky09@users.noreply.github.com>
2019-10-24 00:45:48 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -22,12 +22,12 @@ int main(int argc, char *argv[])
     //allocate memory for the jpg block
     unsigned char buffer [512];
 
+    //start counter, allocate an array for jpg names, and declare the pointer to the image data
     int i = -1;
     char filename [8];
     FILE *img = NULL;
 
-
-
+    //loop start
     do
     {
         //read a raw block and store it in buffer
@@ -42,64 +42,40 @@ int main(int argc, char *argv[])
             //if its not the first jpg encountered
             if(i != 0)
             {
+                //close the opened jpg and continue
                 fclose(img);
             }
 
+            //create the jpg name
             sprintf(filename, "%03i.jpg", i);
-
+            //open the file under jpg name
             img = fopen(filename, "w");
-
+            //write the block from the buffer in that file
             fwrite(buffer, sizeof(buffer), 1, img);
 
         }
-
+        //if we encounter the end of file
         else if(feof(raw))
         {
-
+            //close everything, success
             fclose(img);
 
             fclose(raw);
 
             return 0;
 
-            i = -2;
         }
-
+        //if we havent encountered the first jpg yet, repeat the loop
         else if(i < 0)
         {
             continue;
         }
+        //if the block isnt a start of the jpg, write the buffer to the previously opened jpg
         else
         {
             fwrite(buffer, sizeof(buffer), 1, img);
         }
     }
-    while (i != -2);
-
+    //loop will work until we reach end of raw file
+    while (1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
